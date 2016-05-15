15a20fd12297033d14478409b16c87274a264816
Albino Tonnina <albino.tonnina@workshare.com>
2016-05-15 19:43:11 +0100

    fetch weather action and searchbar

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'd81442fef7bdfd4a8841d3e9475ab8fc';
+//const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country = 'uk') {
+    const url = `${ROOT_URL}&q=${city},${country}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
+
