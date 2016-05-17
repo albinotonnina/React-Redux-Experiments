a7952a05568dbe76b93a532e71b4b5d09030abb7
Albino Tonnina <albino.tonnina@workshare.com>
2016-05-17 22:44:21 +0100

    init

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+// or
+//import { Router, hashHistory } from 'react-router';
+//import { Router, memoryHistory } from 'react-router';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
